eb28c3ea8e1c17926daada668f7e0167d641880c
Pracheer Gupta <guptapra@amazon.com>
2016-02-29 00:02:54 -0800

    Initial Commit

---
+++ HelloIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloIOS
+//
+//  Created by Gupta, Pracheer on 2/29/16.
+//  Copyright (c) 2016 Gupta, Pracheer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
